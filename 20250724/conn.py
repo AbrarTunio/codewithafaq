d7b54ea434b249a63592f6f7fa2c35741a9b03c2
AbrarTunio <abrar.tunio@yahoo.com>
2025-07-24 15:44:25 +0500

    conditions

---
+++ 20250724/conn.py
@@ -0,0 +1,39 @@
+# age = int( input("Enter your age!") ) 
+
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible")
+
+# num = int( input("Enter a number!") )
+# print(  num , type(num)  )
+
+
+# num = int( input("Enter a number") )
+
+# if num % 2 == 0:
+#     print("The number is even")
+# else:
+#     print("The number is odd")
+
+# num = int( input("Enter a number") )
+
+# if num >= 0:
+#     print("Positive Number")
+# else:
+#     print("Negative Number")
+
+# password = input("Enter your password: ")
+
+# if password == "KonDaseendus":
+#     print("Access Granted")
+# else:
+#     print("Access Denied")
+
+
+marks = int( input("Enter your marks") )
+
+if marks >= 33:
+    print("Pass")
+else:
+    print('Fail')
